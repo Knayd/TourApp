61a0aecca5a92cee7ab2e7fe32dc8673c6fe5de3
Deryan Aldair Argueta Hernández <dargueta@applaudostudios.com>
2019-05-19 19:43:49 -0600

    Made Retrofit's instance a singleton.

--- app/src/main/java/com/example/applaudo/tourguideapp/TourApp.java
+++ app/src/main/java/com/example/applaudo/tourguideapp/TourApp.java
@@ -3,9 +3,16 @@ package com.example.applaudo.tourguideapp;
 import android.app.Application;
 import android.content.Context;
 
+import com.example.applaudo.tourguideapp.network.TourApi;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class TourApp extends Application {
 
     private static Context context;
+    private static TourApi tourApi;
 
     @Override
     public void onCreate() {
@@ -13,7 +20,21 @@ public class TourApp extends Application {
         context = getApplicationContext();
     }
 
-    public static Context getContext(){
+    public static Context getContext() {
         return context;
+    }
+
+    public static TourApi getTourApi() {
+
+        if (tourApi == null) {
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl(TourApi.BASE_URL)
+                    .client(new OkHttpClient.Builder().build())
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+            tourApi = retrofit.create(TourApi.class);
+        }
+
+        return tourApi;
     }
 }
