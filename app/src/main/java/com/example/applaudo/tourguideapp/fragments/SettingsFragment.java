56bd092d74a579c6bca3c3d967c05870e3acd812
Deryan Argueta <deryan.hndz@gmail.com>
2019-06-03 01:15:56 -0600

    Added logout button

--- app/src/main/java/com/example/applaudo/tourguideapp/fragments/SettingsFragment.java
+++ app/src/main/java/com/example/applaudo/tourguideapp/fragments/SettingsFragment.java
@@ -2,22 +2,32 @@ package com.example.applaudo.tourguideapp.fragments;
 
 
 import android.os.Bundle;
+import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
+import android.widget.Toast;
 
 import com.example.applaudo.tourguideapp.R;
 import com.example.applaudo.tourguideapp.TourApp;
-import com.example.applaudo.tourguideapp.util.UserPreferences;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
 
-    public SettingsFragment() { }
+    public SettingsFragment() {
+    }
 
     @Override
     public void onCreatePreferences(Bundle bundle, String root) {
         getPreferenceManager().setSharedPreferencesName(TourApp.getContext().getString(R.string.user_preferences));
         setPreferencesFromResource(R.xml.preferences, root);
-    }
 
+        Preference logoutButton = findPreference(getString(R.string.pref_logout));
+        logoutButton.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                Toast.makeText(TourApp.getContext(), "Log out", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+    }
 
 //    @Override
 //    public View onCreateView(LayoutInflater inflater, ViewGroup container,
